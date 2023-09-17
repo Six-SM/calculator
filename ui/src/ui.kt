0650e95952716fb9cab6727c8ae590e61909523c
arnyyyyy <garrybobcrabsburger@gmail.com>
2023-09-18 00:02:19 +0300

    requests history implemented

--- ui/src/ui.kt
+++ ui/src/ui.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -24,13 +25,14 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 
 val symbols = listOf(
     "1",
     "2",
     "3",
     "0",
-    " = ",
+    "=",
     "4",
     "5",
     "6",
@@ -48,121 +50,161 @@ val symbols = listOf(
     "⌫"
 )
 
+val previous_expressions = listOf(
+    "aaa",
+    "bbb",
+    "21479",
+    "2507",
+    "888",
+    "kek",
+    "aaa",
+    "bbb",
+    "21479",
+    "2507",
+    "888",
+    "kek",
+    "aaa",
+    "bbb",
+    "21479",
+    "2507",
+    "888",
+    "kek"
+)
+
+
 @Composable
-fun ButtonClick() {
+fun RunMain() {
     var current_expression by remember { mutableStateOf("") }
     var current_position by remember { mutableStateOf(0) }
 
-    Text(
+    Row {
+        Column(modifier = Modifier.width(400.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                buildAnnotatedString {
+                    append(current_expression.take(current_position))
 
-        buildAnnotatedString {
-            append(current_expression.take(current_position))
-
-            if (current_position < current_expression.length) {
-                withStyle(style = SpanStyle(color = Color.Blue)) {
-                    append(current_expression[current_position])
-                }
-            }
-            if (current_position < current_expression.length - 1) {
-                append(current_expression.substring(current_position + 1, current_expression.length))
-            }
-        },
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .width(150.dp)
-            .absolutePadding(10.dp, 200.dp, 10.dp, 400.dp)
-            .background(Color(0xFFF3E8D3))
-            .padding(30.dp),
-        fontSize = 25.sp,
-        fontWeight = FontWeight.Bold
-
-
-        /*   text = current_expression,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-           .width(150.dp)
-           .absolutePadding(10.dp, 200.dp, 10.dp, 400.dp)
-           .background(Color(0xFFF3E8D3))
-           .padding(30.dp),
-        style = TextStyle(
-           fontSize = 25.sp,
-           fontWeight = FontWeight.Bold
-        ) */
-    )
-
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(5),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .absolutePadding(10.dp, 400.dp, 10.dp, 20.dp)
-    ) {
-        items(symbols.size) {
-            Button(
-                onClick = {
-                    if (it <= 16) {
-                        if (current_position < current_expression.length - 1) {
-                            current_expression =
-                                current_expression.take(current_position + 1) + (symbols[it]) + current_expression.substring(
-                                    current_position + 1,
-                                    current_expression.length
-                                )
-                        } else {
-                            current_expression += symbols[it]
-                        }
-                        current_position++
-                    } else {
-                        if (symbols[it] == "<-") {
-                            current_position--
-                            current_position = maxOf(current_position, 0)
-                        } else if (symbols[it] == "->") {
-                            current_position++
-                            current_position = minOf(current_expression.length - 1, current_position)
-
-
-                        } else if (symbols[it] == "⌫" && current_expression.isNotEmpty()) {
-                            var tmp = current_expression.take(current_position) //взяли все до него
-                            if (current_position < current_expression.length - 1) {
-                                tmp += current_expression.substring(
-                                    current_position + 1,
-                                    current_expression.length
-                                )
-                            }
-                            current_expression = tmp
-                            current_position =
-                                minOf(current_expression.length - 1, current_position)
+                    if (current_position < current_expression.length) {
+                        withStyle(style = SpanStyle(color = Color(0xFFf59563))) {
+                            append(current_expression[current_position])
                         }
                     }
-
-                    if (current_expression.isEmpty()) {
-                        current_position = 0
+                    if (current_position < current_expression.length - 1) {
+                        append(current_expression.substring(current_position + 1, current_expression.length))
                     }
                 },
-
-
-                modifier = Modifier.size(60.dp),
-                colors = ButtonDefaults.buttonColors(Color(0xFFEBD7B2)),
-                shape = RoundedCornerShape(CornerSize(10))
-
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .width(400.dp)
+                    .absolutePadding(10.dp, 100.dp, 10.dp, 0.dp)
+                    .background(Color(0xFFF3E8D3))
+                    .padding(30.dp).height(200.dp),
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
             )
-            {
-                Text(
-                    text = symbols[it],
-                    color = Color.Black,
-                    style = if (!(symbols[it].all { char -> char.isDigit() })) {
-                        TextStyle(fontSize = 23.sp)
-                    } else {
-                        TextStyle(fontSize = 20.sp)
-                    },
-                    textAlign = if (symbols[it] == "⌫") {
-                        TextAlign.Start
-                    } else {
-                        TextAlign.Center
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(5),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .absolutePadding(10.dp, 80.dp, 10.dp, 20.dp)
+            ) {
+                items(symbols.size) {
+                    Button(
+                        onClick = {
+                            if (it <= 16) {
+                                if (current_position < current_expression.length - 1) {
+                                    current_expression =
+                                        current_expression.take(current_position + 1) + (symbols[it]) + current_expression.substring(
+                                            current_position + 1,
+                                            current_expression.length
+                                        )
+                                } else {
+                                    current_expression += symbols[it]
+                                }
+                                current_position++
+                            } else {
+                                if (symbols[it] == "<-") {
+                                    current_position--
+                                    current_position = maxOf(current_position, 0)
+                                } else if (symbols[it] == "->") {
+                                    current_position++
+                                    current_position = minOf(current_expression.length - 1, current_position)
+
+
+                                } else if (symbols[it] == "⌫" && current_expression.isNotEmpty()) {
+                                    var tmp = current_expression.take(current_position)
+                                    if (current_position < current_expression.length - 1) {
+                                        tmp += current_expression.substring(
+                                            current_position + 1,
+                                            current_expression.length
+                                        )
+                                    }
+                                    current_expression = tmp
+                                    current_position =
+                                        minOf(current_expression.length - 1, current_position)
+                                }
+                            }
+
+                            if (current_expression.isEmpty()) {
+                                current_position = 0
+                            }
+                        },
+
+
+                        modifier = Modifier.size(60.dp),
+                        colors = ButtonDefaults.buttonColors(Color(0xFFEBD7B2)),
+                        shape = RoundedCornerShape(CornerSize(10))
+
+                    )
+                    {
+                        Text(
+                            text = symbols[it],
+                            color = Color.Black,
+                            style = if (!(symbols[it].all { char -> char.isDigit() })) {
+                                TextStyle(fontSize = 23.sp)
+                            } else {
+                                TextStyle(fontSize = 20.sp)
+                            },
+                            textAlign = if (symbols[it] == "⌫") {
+                                TextAlign.Start
+                            } else {
+                                TextAlign.Center
+                            }
+                        )
                     }
-                )
+                }
             }
         }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            modifier = Modifier.fillMaxSize().absolutePadding(left = 50.dp, right = 50.dp, top = 90.dp, bottom = 60.dp),
+            content = {
+                items(previous_expressions.size) {
+                    Button(
+                        onClick = {}, //TODO
+                        modifier = Modifier
+                            .width(400.dp)
+                            .absolutePadding(10.dp, 10.dp, 10.dp, 0.dp)
+                            .background(Color(0xFFF3E8D3))
+                            .height(60.dp),
+                        colors = ButtonDefaults.buttonColors(Color(0xFFF3E8D3)),
+                    ) {
+
+                        Text(
+                            previous_expressions[it],
+                            fontSize = 25.sp,
+                        )
+
+                    }
+                }
+
+            }
+        )
     }
 }
 
@@ -174,13 +216,14 @@ fun App() {
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFFF9F2E7),
     ) {
-        ButtonClick()
+        RunMain()
     }
 }
 
 fun main() = application {
-    val windowState = rememberWindowState(height = 1000.dp, width = 700.dp)
+    val windowState = rememberWindowState(height = 800.dp, width = 900.dp)
     Window(onCloseRequest = ::exitApplication, state = windowState) {
+        window.minimumSize = Dimension(900, 800)
         App()
     }
 }
